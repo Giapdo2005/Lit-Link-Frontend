0283fa2b0509372e6988e5223fa2dd35ed6996b9
Giap Do <giapdo1901@gmail.com>
2025-01-02 10:03:22 -0800

    api route changed

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://lit-link-backend-4.onrender.com/api";
 
 // get all books from database
 export const fetchBooks = async (id) => {
